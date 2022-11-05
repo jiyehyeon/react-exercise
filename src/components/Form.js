6f85842ca736f93ca973b86becc663fb2562bd6b
[AI5/1반] 현지예 <hyeonjiyeah@gmail.com>
2022-11-05 18:39:23 +0900

    스타일 변경

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+
+const InputStyle = styled.input`
+  width: 85%;
+  padding: 10px;
+  border-radius: 5px;
+  border: lightgrey 1px solid;
+`;
+
+const BtnStyle = styled.button`
+  width: 10%;
+  padding: 11px;
+  border: none;
+  border-radius: 5px;
+`;
+
+const InputWrapper = styled.div`
+  height: 45px;
+  margin-top: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 export default function Form({ handleSubmit, value, setValue }) {
   const handleChange = (e) => {
@@ -7,21 +30,17 @@ export default function Form({ handleSubmit, value, setValue }) {
 
   return (
     <div>
-      <form style={{ display: "flex " }} onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="value"
-          style={{ flex: "10", padding: "5px" }}
-          placeholder="해야 할 일을 입력하세요."
-          value={value}
-          onChange={handleChange}
-        />
-        <input
-          type="submit"
-          value="입력"
-          className="btn"
-          style={{ flex: "1" }}
-        />
+      <form onSubmit={handleSubmit}>
+        <InputWrapper>
+          <InputStyle
+            type="text"
+            name="value"
+            placeholder="해야 할 일을 입력하세요."
+            value={value}
+            onChange={handleChange}
+          />
+          <BtnStyle type="submit">입력</BtnStyle>
+        </InputWrapper>
       </form>
     </div>
   );
