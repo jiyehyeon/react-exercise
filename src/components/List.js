9786fc466d7ca0134ab55ea9966d2ba83fc41609
[AI5/1반] 현지예 <hyeonjiyeah@gmail.com>
2022-11-05 21:29:13 +0900

    Drag and Drop 기능 구현

--- src/components/List.js
+++ src/components/List.js
@@ -1,15 +1,34 @@
 import React from "react";
+import styled from "styled-components";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-export default function List({ todoData, setTodoData }) {
-  const btnStyle = {
-    color: "#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right",
-  };
+const BtnStyle = styled.button`
+  position: absolute;
+  right: 10px;
+  background-color: rgba(0, 0, 0, 0.1);
+  color: #fff;
+  font-weight: 600;
+  border: none;
+  padding: 5px 10px 6px 10px;
+  border-radius: 50%;
+  float: right;
+`;
 
+const ListStyle = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  padding: 10px;
+  border-bottom: 1px #ccc dotted;
+  text-decoration: ${({ completed }) => (completed ? "line-through" : "none")};
+
+  & span {
+    margin-left: 5px;
+  }
+`;
+
+function List({ todoData, setTodoData }) {
   const handleCompleteChange = (id) => {
     const newTodoData = todoData.map((data) => {
       if (data.id === id) {
@@ -25,29 +44,57 @@ export default function List({ todoData, setTodoData }) {
     setTodoData(newTodoData);
   };
 
-  const listStyle = (completed) => {
-    return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed ? "line-through" : "none",
-    };
+  const handleEnd = (result) => {
+    if (!result.destination) return;
+
+    const changedData = todoData;
+
+    const [reorderedItem] = changedData.splice(result.source.index, 1);
+
+    changedData.splice(result.destination.index, 0, reorderedItem);
+    setTodoData(changedData);
   };
 
   return (
     <div>
-      {todoData.map((data) => (
-        <div style={listStyle(data.completed)} key={data.id}>
-          <input
-            onChange={() => handleCompleteChange(data.id)}
-            type="checkbox"
-            defaultChecked={false}
-          />
-          {data.title}
-          <button style={btnStyle} onClick={() => handleClick(data.id)}>
-            x
-          </button>
-        </div>
-      ))}
+      <DragDropContext onDragEnd={handleEnd}>
+        <Droppable droppableId="todo">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {todoData.map((data, index) => (
+                <Draggable
+                  key={data.id}
+                  draggableId={data.id.toString()}
+                  index={index}
+                >
+                  {(provided, snapshot) => (
+                    <ListStyle
+                      key={data.id}
+                      {...provided.draggableProps}
+                      ref={provided.innerRef}
+                      {...provided.dragHandleProps}
+                      completed={data.completed}
+                    >
+                      <input
+                        onChange={() => handleCompleteChange(data.id)}
+                        type="checkbox"
+                        defaultChecked={false}
+                      />
+                      <span>{data.title}</span>
+                      <BtnStyle onClick={() => handleClick(data.id)}>
+                        x
+                      </BtnStyle>
+                    </ListStyle>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
+
+export default List;
